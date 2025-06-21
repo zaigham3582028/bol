d29b585a055c3a045c7e9ed3bba826ad9e2e611d
zaigham3582028 <165129563+zaigham3582028@users.noreply.github.com>
2025-06-21 16:17:01 +0500

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { enableMapSet } from 'immer';
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+// Enable Immer's MapSet plugin to handle Set and Map objects
+enableMapSet();
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </StrictMode>
+);
